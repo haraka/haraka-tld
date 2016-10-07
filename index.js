22fe118cc77d51a9ccd83d5e319f326644f71ef2
Matt Simerson <matt@tnpi.net>
2016-10-06 23:41:24 -0700

    oct update, code climate (#17)
    * oct update, code climate

--- index.js
+++ index.js
@@ -8,40 +8,40 @@ var path     = require('path');
 var punycode = require('punycode');
 
 var regex = {
-    comment:        /^\s*[;#].*$/,
-    blank:          /^\s*$/,
-    line:           /^\s*(.*?)\s*$/,
+  comment:        /^\s*[;#].*$/,
+  blank:          /^\s*$/,
+  line:           /^\s*(.*?)\s*$/,
 };
 
 module.exports = exports = {
-    public_suffix_list: {},
-    top_level_tlds: {},
-    two_level_tlds: {},
-    three_level_tlds: {},
+  public_suffix_list: {},
+  top_level_tlds: {},
+  two_level_tlds: {},
+  three_level_tlds: {},
 };
 
 exports.is_public_suffix = function (host) {
-    if (!host) return false;
-    host = host.toLowerCase();
-    if (exports.public_suffix_list[host]) return true;
+  if (!host) return false;
+  host = host.toLowerCase();
+  if (exports.public_suffix_list[host]) return true;
 
-    var up_one_level = host.split('.').slice(1).join('.'); // co.uk -> uk
-    if (!up_one_level) return false;   // no dot?
+  var up_one_level = host.split('.').slice(1).join('.'); // co.uk -> uk
+  if (!up_one_level) return false;   // no dot?
 
-    var wildHost = '*.' + up_one_level;
-    if (exports.public_suffix_list[wildHost]) {
+  var wildHost = '*.' + up_one_level;
+  if (exports.public_suffix_list[wildHost]) {
         // check exception list
-        if (exports.public_suffix_list['!'+host]) return false;
-        return true;           // matched a wildcard, ex: *.uk
-    }
+    if (exports.public_suffix_list['!'+host]) return false;
+    return true;           // matched a wildcard, ex: *.uk
+  }
 
-    var puny;
-    try { puny = punycode.toUnicode(host); }
+  var puny;
+  try { puny = punycode.toUnicode(host); }
     catch (ignore) {}
 
-    if (puny && exports.public_suffix_list[puny]) return true;
+  if (puny && exports.public_suffix_list[puny]) return true;
 
-    return false;
+  return false;
 };
 
 exports.get_organizational_domain = function (host) {
@@ -49,123 +49,123 @@ exports.get_organizational_domain = function (host) {
     // https://datatracker.ietf.org/doc/draft-kucherawy-dmarc-base/?include_text=1
     //   section 3.2
 
-    if (!host) return null;
-    host = host.toLowerCase();
+  if (!host) return null;
+  host = host.toLowerCase();
 
     // www.example.com -> [ com, example, www ]
-    var labels = host.split('.').reverse();
+  var labels = host.split('.').reverse();
 
     // 4.3 Search the public suffix list for the name that matches the
     //     largest number of labels found in the subject DNS domain.
-    var greatest = 0;
-    for (var i = 1; i <= labels.length; i++) {
-        if (!labels[i-1]) return null;                   // dot w/o label
-        var tld = labels.slice(0,i).reverse().join('.');
-        if (exports.is_public_suffix(tld)) {
-            greatest = +(i + 1);
-        }
-        else if (exports.public_suffix_list['!'+tld]) {
-            greatest = i;
-        }
+  var greatest = 0;
+  for (var i = 1; i <= labels.length; i++) {
+    if (!labels[i-1]) return null;                   // dot w/o label
+    var tld = labels.slice(0,i).reverse().join('.');
+    if (exports.is_public_suffix(tld)) {
+      greatest = +(i + 1);
     }
+    else if (exports.public_suffix_list['!'+tld]) {
+      greatest = i;
+    }
+  }
 
     // 4.4 Construct a new DNS domain name using the name that matched
     //     from the public suffix list and prefixing to it the "x+1"th
     //     label from the subject domain.
-    if (greatest === 0) return null;             // no valid TLD
-    if (greatest  >  labels.length) return null; // not enough labels
-    if (greatest === labels.length) return host; // same
+  if (greatest === 0) return null;             // no valid TLD
+  if (greatest  >  labels.length) return null; // not enough labels
+  if (greatest === labels.length) return host; // same
 
-    var orgName = labels.slice(0, greatest).reverse().join('.');
-    return orgName;
+  var orgName = labels.slice(0, greatest).reverse().join('.');
+  return orgName;
 };
 
 exports.split_hostname = function (host, level) {
-    if (!level || (level && !(level >= 1 && level <= 3))) {
-        level = 2;
-    }
+  if (!level || (level && !(level >= 1 && level <= 3))) {
+    level = 2;
+  }
 
-    var split = host.toLowerCase().split(/\./).reverse();
-    var domain = '';
+  var split = host.toLowerCase().split(/\./).reverse();
+  var domain = '';
     // TLD
-    if (level >= 1 && split[0] && exports.top_level_tlds[split[0]]) {
-        domain = split.shift() + domain;
-    }
+  if (level >= 1 && split[0] && exports.top_level_tlds[split[0]]) {
+    domain = split.shift() + domain;
+  }
     // 2nd TLD
-    if (level >= 2 && split[0] && exports.two_level_tlds[split[0] + '.' + domain]) {
-        domain = split.shift() + '.' + domain;
-    }
+  if (level >= 2 && split[0] && exports.two_level_tlds[split[0] + '.' + domain]) {
+    domain = split.shift() + '.' + domain;
+  }
     // 3rd TLD
-    if (level >= 3 && split[0] && exports.three_level_tlds[split[0] + '.' + domain]) {
-        domain = split.shift() + '.' + domain;
-    }
+  if (level >= 3 && split[0] && exports.three_level_tlds[split[0] + '.' + domain]) {
+    domain = split.shift() + '.' + domain;
+  }
     // Domain
-    if (split[0]) {
-        domain = split.shift() + '.' + domain;
-    }
-    return [split.reverse().join('.'), domain];
+  if (split[0]) {
+    domain = split.shift() + '.' + domain;
+  }
+  return [split.reverse().join('.'), domain];
 };
 
 function load_public_suffix_list() {
-    load_list_from_file('public-suffix-list').forEach(function (entry) {
+  load_list_from_file('public-suffix-list').forEach(function (entry) {
         // Parsing rules: http://publicsuffix.org/list/
         // Each line is only read up to the first whitespace
-        var suffix = entry.split(/\s/).shift();
+    var suffix = entry.split(/\s/).shift();
 
         // Each line which is not entirely whitespace or begins with a comment
         // contains a rule.
-        if (!suffix) return;                            // empty string
-        if ('/' === suffix.substring(0,1)) return;      // comment
+    if (!suffix) return;                            // empty string
+    if ('/' === suffix.substring(0,1)) return;      // comment
 
         // A rule may begin with a "!" (exclamation mark). If it does, it is
         // labelled as a "exception rule" and then treated as if the exclamation
         // mark is not present.
-        if ('!' === suffix.substring(0,1)) {
-            var eName = suffix.substring(1);   // remove ! prefix
+    if ('!' === suffix.substring(0,1)) {
+      var eName = suffix.substring(1);   // remove ! prefix
             // bbc.co.uk -> co.uk
-            var up_one = suffix.split('.').slice(1).join('.');
-            if (exports.public_suffix_list[up_one]) {
-                exports.public_suffix_list[up_one].push(eName);
-            }
-            else if (exports.public_suffix_list['*.'+up_one]) {
-                exports.public_suffix_list['*.'+up_one].push(eName);
-            }
-            else {
-                console.error('unable to find parent for exception: ' + eName);
-            }
-        }
+      var up_one = suffix.split('.').slice(1).join('.');
+      if (exports.public_suffix_list[up_one]) {
+        exports.public_suffix_list[up_one].push(eName);
+      }
+      else if (exports.public_suffix_list['*.'+up_one]) {
+        exports.public_suffix_list['*.'+up_one].push(eName);
+      }
+      else {
+        console.error('unable to find parent for exception: ' + eName);
+      }
+    }
 
-        exports.public_suffix_list[suffix] = [];
-    });
+    exports.public_suffix_list[suffix] = [];
+  });
 
-    console.log('loaded '+ Object.keys(exports.public_suffix_list).length +
+  console.log('loaded '+ Object.keys(exports.public_suffix_list).length +
         ' Public Suffixes');
 }
 
 function load_tld_files () {
-    load_list_from_file('top-level-tlds').forEach(function (tld) {
-        exports.top_level_tlds[tld] = 1;
-    });
+  load_list_from_file('top-level-tlds').forEach(function (tld) {
+    exports.top_level_tlds[tld] = 1;
+  });
 
-    load_list_from_file('two-level-tlds').forEach(function (tld) {
-        exports.two_level_tlds[tld] = 1;
-    });
+  load_list_from_file('two-level-tlds').forEach(function (tld) {
+    exports.two_level_tlds[tld] = 1;
+  });
 
-    load_list_from_file('three-level-tlds').forEach(function (tld) {
-        exports.three_level_tlds[tld] = 1;
-    });
+  load_list_from_file('three-level-tlds').forEach(function (tld) {
+    exports.three_level_tlds[tld] = 1;
+  });
 
-    load_list_from_file('extra-tlds').forEach(function (tld) {
-        var s = tld.split(/\./);
-        if (s.length === 2) {
-            exports.two_level_tlds[tld] = 1;
-        }
-        else if (s.length === 3) {
-            exports.three_level_tlds[tld] = 1;
-        }
-    });
+  load_list_from_file('extra-tlds').forEach(function (tld) {
+    var s = tld.split(/\./);
+    if (s.length === 2) {
+      exports.two_level_tlds[tld] = 1;
+    }
+    else if (s.length === 3) {
+      exports.three_level_tlds[tld] = 1;
+    }
+  });
 
-    console.log('loaded TLD files:' +
+  console.log('loaded TLD files:' +
     ' 1=' + Object.keys(exports.top_level_tlds).length +
     ' 2=' + Object.keys(exports.two_level_tlds).length +
     ' 3=' + Object.keys(exports.three_level_tlds).length
@@ -173,27 +173,27 @@ function load_tld_files () {
 }
 
 function load_list_from_file (name) {
-    var result = [];
+  var result = [];
 
-    var filePath = path.resolve(__dirname, 'etc', name);
-    if (!fs.existsSync(filePath)) {
+  var filePath = path.resolve(__dirname, 'etc', name);
+  if (!fs.existsSync(filePath)) {
         // not loaded by Haraka, use local path
-        filePath = path.resolve('etc', name);
-    }
+    filePath = path.resolve('etc', name);
+  }
 
-    fs.readFileSync(filePath, 'UTF-8')
+  fs.readFileSync(filePath, 'UTF-8')
     .split(/\r\n|\r|\n/)
     .forEach(function(line) {
 
-        if (regex.comment.test(line)) return;
-        if (regex.blank.test(line))   return;
+      if (regex.comment.test(line)) return;
+      if (regex.blank.test(line))   return;
 
-        var line_data = regex.line.exec(line);
-        if (!line_data) return;
+      var line_data = regex.line.exec(line);
+      if (!line_data) return;
 
-        result.push(line_data[1].trim().toLowerCase());
+      result.push(line_data[1].trim().toLowerCase());
     });
-    return result;
+  return result;
 }
 
 load_tld_files();
--- test/index.js
+++ test/index.js
@@ -5,176 +5,176 @@ var assert = require('assert');
 var tlds = require('../index');
 
 describe('haraka-tld', function () {
-    it('exports lists with reasonable qty', function (done) {
+  it('exports lists with reasonable qty', function (done) {
         // console.log(tlds);
-        assert.ok(Object.keys(tlds.public_suffix_list).length > 7000);
-        assert.ok(Object.keys(tlds.top_level_tlds).length > 1000);
-        assert.ok(Object.keys(tlds.two_level_tlds).length > 5000);
-        assert.ok(Object.keys(tlds.three_level_tlds).length > 2000);
-        done();
-    });
+    assert.ok(Object.keys(tlds.public_suffix_list).length > 7000);
+    assert.ok(Object.keys(tlds.top_level_tlds).length > 1000);
+    assert.ok(Object.keys(tlds.two_level_tlds).length > 5000);
+    assert.ok(Object.keys(tlds.three_level_tlds).length > 2000);
+    done();
+  });
 });
 
 var od_test_cases = {
-    null: [ null, null ],
+  null: [ null, null ],
 
     // Mixed case.
-    COM: [ 'COM', null],
-    'example.COM': [ 'example.COM', 'example.com'],
-    'WwW.example.COM': [ 'WwW.example.COM', 'example.com'],
+  COM: [ 'COM', null],
+  'example.COM': [ 'example.COM', 'example.com'],
+  'WwW.example.COM': [ 'WwW.example.COM', 'example.com'],
 
     // Leading dot.
-    '.com': [ '.com', null],
-    '.example': [ '.example', null],
-    '.example.com': [ '.example.com', null],
-    '.example.example': [ '.example.example', null],
+  '.com': [ '.com', null],
+  '.example': [ '.example', null],
+  '.example.com': [ '.example.com', null],
+  '.example.example': [ '.example.example', null],
 
     // Unlisted TLD.
-    'example': [ 'example', null],
-    'example.example': [ 'example.example', null],
+  'example': [ 'example', null],
+  'example.example': [ 'example.example', null],
     // _org_domain(test, 'b.example.example', 'example.example');
     // _org_domain(test, 'a.b.example.example', 'example.example');
 
     // Listed, but non-Internet, TLD.
-    'local': [ 'local', null],
-    'example.local': [ 'example.local', null],
-    'b.example.local': [ 'b.example.local', null],
-    'a.b.example.local': [ 'a.b.example.local', null],
+  'local': [ 'local', null],
+  'example.local': [ 'example.local', null],
+  'b.example.local': [ 'b.example.local', null],
+  'a.b.example.local': [ 'a.b.example.local', null],
 
     // TLD with only 1 rule.
-    'biz': [ 'biz', null],
-    'domain.biz': [ 'domain.biz', 'domain.biz'],
-    'b.domain.biz': [ 'b.domain.biz', 'domain.biz'],
-    'a.b.domain.biz': [ 'a.b.domain.biz', 'domain.biz'],
+  'biz': [ 'biz', null],
+  'domain.biz': [ 'domain.biz', 'domain.biz'],
+  'b.domain.biz': [ 'b.domain.biz', 'domain.biz'],
+  'a.b.domain.biz': [ 'a.b.domain.biz', 'domain.biz'],
 
-    'com': [ 'com', null],
-    'example.com': [ 'example.com', 'example.com'],
-    'b.example.com': [ 'b.example.com', 'example.com'],
-    'a.b.example.com': [ 'a.b.example.com', 'example.com'],
-    'uk.com': [ 'uk.com', null],
-    'example.uk.com': [ 'example.uk.com', 'example.uk.com'],
-    'b.example.uk.com': [ 'b.example.uk.com', 'example.uk.com'],
-    'a.b.example.uk.com': [ 'a.b.example.uk.com', 'example.uk.com'],
-    'test.ac': [ 'test.ac', 'test.ac'],
+  'com': [ 'com', null],
+  'example.com': [ 'example.com', 'example.com'],
+  'b.example.com': [ 'b.example.com', 'example.com'],
+  'a.b.example.com': [ 'a.b.example.com', 'example.com'],
+  'uk.com': [ 'uk.com', null],
+  'example.uk.com': [ 'example.uk.com', 'example.uk.com'],
+  'b.example.uk.com': [ 'b.example.uk.com', 'example.uk.com'],
+  'a.b.example.uk.com': [ 'a.b.example.uk.com', 'example.uk.com'],
+  'test.ac': [ 'test.ac', 'test.ac'],
 
     // TLD with some 2-level rules.
     // TLD with only 1 (wildcard) rule.
-    'cy': [ 'cy', null],
-    'c.cy': [ 'c.cy', null],
-    'b.c.cy': [ 'b.c.cy', null],
-    'a.b.c.cy': [ 'a.b.c.cy', null],
+  'cy': [ 'cy', null],
+  'c.cy': [ 'c.cy', null],
+  'b.c.cy': [ 'b.c.cy', null],
+  'a.b.c.cy': [ 'a.b.c.cy', null],
 
     // More complex TLD.
-    'jp': [ 'jp', null],
-    'test.jp': [ 'test.jp', 'test.jp'],
-    'www.test.jp': [ 'www.test.jp', 'test.jp'],
-    'ac.jp': [ 'ac.jp', null],
-    'test.ac.jp': [ 'test.ac.jp', 'test.ac.jp'],
-    'www.test.ac.jp': [ 'www.test.ac.jp', 'test.ac.jp'],
-    'kyoto.jp': [ 'kyoto.jp', null],
-    'test.kyoto.jp': [ 'test.kyoto.jp', 'test.kyoto.jp'],
-    'ide.kyoto.jp': [ 'ide.kyoto.jp', null],
-    'b.ide.kyoto.jp': [ 'b.ide.kyoto.jp', 'b.ide.kyoto.jp'],
-    'a.b.ide.kyoto.jp': [ 'a.b.ide.kyoto.jp', 'b.ide.kyoto.jp'],
-    'c.kobe.jp': [ 'c.kobe.jp', null],
-    'b.c.kobe.jp': [ 'b.c.kobe.jp', 'b.c.kobe.jp'],
-    'a.b.c.kobe.jp': [ 'a.b.c.kobe.jp', 'b.c.kobe.jp'],
-    'city.kobe.jp': [ 'city.kobe.jp', 'city.kobe.jp'],
-    'www.city.kobe.jp': [ 'www.city.kobe.jp', 'city.kobe.jp'],
+  'jp': [ 'jp', null],
+  'test.jp': [ 'test.jp', 'test.jp'],
+  'www.test.jp': [ 'www.test.jp', 'test.jp'],
+  'ac.jp': [ 'ac.jp', null],
+  'test.ac.jp': [ 'test.ac.jp', 'test.ac.jp'],
+  'www.test.ac.jp': [ 'www.test.ac.jp', 'test.ac.jp'],
+  'kyoto.jp': [ 'kyoto.jp', null],
+  'test.kyoto.jp': [ 'test.kyoto.jp', 'test.kyoto.jp'],
+  'ide.kyoto.jp': [ 'ide.kyoto.jp', null],
+  'b.ide.kyoto.jp': [ 'b.ide.kyoto.jp', 'b.ide.kyoto.jp'],
+  'a.b.ide.kyoto.jp': [ 'a.b.ide.kyoto.jp', 'b.ide.kyoto.jp'],
+  'c.kobe.jp': [ 'c.kobe.jp', null],
+  'b.c.kobe.jp': [ 'b.c.kobe.jp', 'b.c.kobe.jp'],
+  'a.b.c.kobe.jp': [ 'a.b.c.kobe.jp', 'b.c.kobe.jp'],
+  'city.kobe.jp': [ 'city.kobe.jp', 'city.kobe.jp'],
+  'www.city.kobe.jp': [ 'www.city.kobe.jp', 'city.kobe.jp'],
 
     // TLD with a wildcard rule and exceptions.
-    'ck': [ 'ck', null],
-    'test.ck': [ 'test.ck', null],
-    'b.test.ck': [ 'b.test.ck', 'b.test.ck'],
-    'a.b.test.ck': [ 'a.b.test.ck', 'b.test.ck'],
-    'www.ck': [ 'www.ck', 'www.ck'],
-    'www.www.ck': [ 'www.www.ck', 'www.ck'],
+  'ck': [ 'ck', null],
+  'test.ck': [ 'test.ck', null],
+  'b.test.ck': [ 'b.test.ck', 'b.test.ck'],
+  'a.b.test.ck': [ 'a.b.test.ck', 'b.test.ck'],
+  'www.ck': [ 'www.ck', 'www.ck'],
+  'www.www.ck': [ 'www.www.ck', 'www.ck'],
     // US K12.
-    'us': [ 'us', null],
-    'test.us': [ 'test.us', 'test.us'],
-    'www.test.us': [ 'www.test.us', 'test.us'],
-    'ak.us': [ 'ak.us', null],
-    'test.ak.us': [ 'test.ak.us', 'test.ak.us'],
-    'www.test.ak.us': [ 'www.test.ak.us', 'test.ak.us'],
-    'k12.ak.us': [ 'k12.ak.us', null],
-    'test.k12.ak.us': [ 'test.k12.ak.us', 'test.k12.ak.us'],
-    'www.test.k12.ak.us': [ 'www.test.k12.ak.us', 'test.k12.ak.us'],
+  'us': [ 'us', null],
+  'test.us': [ 'test.us', 'test.us'],
+  'www.test.us': [ 'www.test.us', 'test.us'],
+  'ak.us': [ 'ak.us', null],
+  'test.ak.us': [ 'test.ak.us', 'test.ak.us'],
+  'www.test.ak.us': [ 'www.test.ak.us', 'test.ak.us'],
+  'k12.ak.us': [ 'k12.ak.us', null],
+  'test.k12.ak.us': [ 'test.k12.ak.us', 'test.k12.ak.us'],
+  'www.test.k12.ak.us': [ 'www.test.k12.ak.us', 'test.k12.ak.us'],
 
     // IDN labels.
-    '食狮.com.cn': [ '食狮.com.cn', '食狮.com.cn'],
-    '食狮.公司.cn': [ '食狮.公司.cn', '食狮.公司.cn'],
-    'www.食狮.公司.cn': [ 'www.食狮.公司.cn', '食狮.公司.cn'],
-    'shishi.公司.cn': [ 'shishi.公司.cn', 'shishi.公司.cn'],
-    '公司.cn': [ '公司.cn', null],
-    '食狮.中国': [ '食狮.中国', '食狮.中国'],
-    'www.食狮.中�': [ 'www.食狮.中国', '食狮.中国'],
-    'shishi.中国': [ 'shishi.中国', 'shishi.中国'],
-    '中国': [ '中国', null],
+  '食狮.com.cn': [ '食狮.com.cn', '食狮.com.cn'],
+  '食狮.公司.cn': [ '食狮.公司.cn', '食狮.公司.cn'],
+  'www.食狮.公司.cn': [ 'www.食狮.公司.cn', '食狮.公司.cn'],
+  'shishi.公司.cn': [ 'shishi.公司.cn', 'shishi.公司.cn'],
+  '公司.cn': [ '公司.cn', null],
+  '食狮.中国': [ '食狮.中国', '食狮.中国'],
+  'www.食狮.中�': [ 'www.食狮.中国', '食狮.中国'],
+  'shishi.中国': [ 'shishi.中国', 'shishi.中国'],
+  '中国': [ '中国', null],
 
     // Same as above, but punycoded.
-    'xn--85x722f.com.cn': [ 'xn--85x722f.com.cn', 'xn--85x722f.com.cn'],
-    'xn--85x722f.xn--55qx5d.cn': [ 'xn--85x722f.xn--55qx5d.cn',
+  'xn--85x722f.com.cn': [ 'xn--85x722f.com.cn', 'xn--85x722f.com.cn'],
+  'xn--85x722f.xn--55qx5d.cn': [ 'xn--85x722f.xn--55qx5d.cn',
         'xn--85x722f.xn--55qx5d.cn'],
-    'www.xn--85x722f.xn--55qx5d.cn': [ 'www.xn--85x722f.xn--55qx5d.cn',
+  'www.xn--85x722f.xn--55qx5d.cn': [ 'www.xn--85x722f.xn--55qx5d.cn',
             'xn--85x722f.xn--55qx5d.cn'],
-    'shishi.xn--55qx5d.cn': [ 'shishi.xn--55qx5d.cn', 'shishi.xn--55qx5d.cn'],
-    'xn--55qx5d.cn': [ 'xn--55qx5d.cn', null],
+  'shishi.xn--55qx5d.cn': [ 'shishi.xn--55qx5d.cn', 'shishi.xn--55qx5d.cn'],
+  'xn--55qx5d.cn': [ 'xn--55qx5d.cn', null],
     // 'xn--85x722f.xn--fiqs8s': [ 'xn--85x722f.xn--fiqs8s',
     //     'xn--85x722f.xn--fiqs8s'],
     // 'www.xn--85x722f.xn--fiqs8s': [ 'www.xn--85x722f.xn--fiqs8s',
     //         'xn--85x722f.xn--fiqs8s'],
     // 'shishi.xn--fiqs8s': [ 'shishi.xn--fiqs8s', 'shishi.xn--fiqs8s'],
-    'xn--fiqs8s': [ 'xn--fiqs8s', null],
+  'xn--fiqs8s': [ 'xn--fiqs8s', null],
 };
 
 describe('get_organizational_domain, test suite', function () {
-    Object.keys(od_test_cases).forEach(function (descr) {
-        var tc = od_test_cases[descr];
-        it(descr, function () {
-            assert.equal(tlds.get_organizational_domain(tc[0]), tc[1]);
-        });
+  Object.keys(od_test_cases).forEach(function (descr) {
+    var tc = od_test_cases[descr];
+    it(descr, function () {
+      assert.equal(tlds.get_organizational_domain(tc[0]), tc[1]);
     });
+  });
 });
 
 var ps_test_cases = {
-    'com': [ 'com', true ],
-    'COM (uc)': [ 'COM', true ],
-    'net': [ 'net', true ],
-    'co.uk': [ 'co.uk', true ],
-    'org': [ 'org', true ],
-    'edu': [ 'edu', true ],
-    'gov': [ 'gov', true ],
-    'empty': [ '', false ],
-    'null': [ '', false ],
+  'com': [ 'com', true ],
+  'COM (uc)': [ 'COM', true ],
+  'net': [ 'net', true ],
+  'co.uk': [ 'co.uk', true ],
+  'org': [ 'org', true ],
+  'edu': [ 'edu', true ],
+  'gov': [ 'gov', true ],
+  'empty': [ '', false ],
+  'null': [ '', false ],
 };
 
 describe('is_public_suffix', function () {
-    Object.keys(ps_test_cases).forEach(function (descr) {
-        var tc = ps_test_cases[descr];
-        it(descr, function () {
-            assert.equal(tlds.is_public_suffix(tc[0]), tc[1]);
-        });
+  Object.keys(ps_test_cases).forEach(function (descr) {
+    var tc = ps_test_cases[descr];
+    it(descr, function () {
+      assert.equal(tlds.is_public_suffix(tc[0]), tc[1]);
     });
+  });
 });
 
 describe('split_hostname', function () {
-    it('splits on domain boundary', function () {
-        var foo = tlds.split_hostname('host.sub1.sub2.domain.com');
-        assert.equal(foo[0],'host.sub1.sub2');
-        assert.equal(foo[1],'domain.com');
+  it('splits on domain boundary', function () {
+    var foo = tlds.split_hostname('host.sub1.sub2.domain.com');
+    assert.equal(foo[0],'host.sub1.sub2');
+    assert.equal(foo[1],'domain.com');
+  });
+  [1,2,3].forEach(function (level) {
+    it('splits on domain boundary, level ' + level, function () {
+      var foo = tlds.split_hostname('host.sub1.sub2.domain.com', level);
+      assert.equal(foo[0],'host.sub1.sub2');
+      assert.equal(foo[1],'domain.com');
     });
-    [1,2,3].forEach(function (level) {
-        it('splits on domain boundary, level ' + level, function () {
-            var foo = tlds.split_hostname('host.sub1.sub2.domain.com', level);
-            assert.equal(foo[0],'host.sub1.sub2');
-            assert.equal(foo[1],'domain.com');
-        });
-    });
-    it('splits empty host on TLD only', function () {
-        assert.deepEqual(tlds.split_hostname('com'), ['', 'com']);
-    });
-    it('splits a 3-level TLD', function () {
-        assert.deepEqual(tlds.split_hostname('host.b.topica.com', 4),
+  });
+  it('splits empty host on TLD only', function () {
+    assert.deepEqual(tlds.split_hostname('com'), ['', 'com']);
+  });
+  it('splits a 3-level TLD', function () {
+    assert.deepEqual(tlds.split_hostname('host.b.topica.com', 4),
         ['host', 'b.topica.com']);
-    });
+  });
 });
