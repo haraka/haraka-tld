acf6455df24d3e7169e66fac05bec70ceedb8dbb
Matt Simerson <matt@tnpi.net>
2016-10-06 23:31:21 -0700

    fix ws in Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,41 +1,41 @@
 
 module.exports = function(grunt) {
 
-    grunt.loadNpmTasks('grunt-eslint');
-    grunt.loadNpmTasks('grunt-mocha-test');
-    grunt.loadNpmTasks('grunt-release');
-    grunt.loadNpmTasks('grunt-version-check');
+  grunt.loadNpmTasks('grunt-eslint');
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-release');
+  grunt.loadNpmTasks('grunt-version-check');
 
     // Project configuration.
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        eslint: {
-            main: {
-                src: ['Gruntfile.js','index.js','lib/**/*.js']
-            },
-            test: {
-                src: ['test/**/*.js'],
-            }
-        },
-        mochaTest: {
-            options: {
-            },
-            any: {
-                src: ['test/**/*.js']
-            }
-        },
-        versioncheck: {
-          target: {
-            options: {
-                skip: ['semver', 'npm', 'lodash'],
-                hideUpToDate: false
-            }
-          }
-        },
-    });
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    eslint: {
+      main: {
+        src: ['Gruntfile.js','index.js','lib/**/*.js']
+      },
+      test: {
+        src: ['test/**/*.js'],
+      }
+    },
+    mochaTest: {
+      options: {
+      },
+      any: {
+        src: ['test/**/*.js']
+      }
+    },
+    versioncheck: {
+      target: {
+        options: {
+          skip: ['semver', 'npm', 'lodash'],
+          hideUpToDate: false
+        }
+      }
+    },
+  });
 
-    grunt.registerTask('lint',    ['eslint']);
-    grunt.registerTask('test',    ['mochaTest']);
-    grunt.registerTask('ver',     ['versioncheck']);
-    grunt.registerTask('default', ['eslint','mochaTest']);
+  grunt.registerTask('lint',    ['eslint']);
+  grunt.registerTask('test',    ['mochaTest']);
+  grunt.registerTask('ver',     ['versioncheck']);
+  grunt.registerTask('default', ['eslint','mochaTest']);
 };
