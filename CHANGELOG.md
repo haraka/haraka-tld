8a6fa686aa0979cad56648761ca70936689755ae
Matt Simerson <matt@tnpi.net>
2024-09-27 15:28:06 -0700

    automated code formatting

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,11 +1,8 @@
-
 ### Unreleased
-
 
 ### [1.2.2] - 2024-09-27
 
 - updated TLD files
-
 
 ### [1.2.1] - 2024-04-03
 
@@ -15,24 +12,20 @@
 - dep(mocha & eslint): remove from devDeps (install with npx)
 - pkg: add ./test to .npmignore
 
-
 ### [1.2.0] - 2023-12-12
 
 - dep(punycode): correctly specify (with trailing /)
-    - to override built-in, which emits warnings
-
+  - to override built-in, which emits warnings
 
 ### [1.1.2] - 2023-12-11
 
 - updated TLD files
 - Update README.md (#78)
 
-
 ### [1.1.1] - 2023-06-16
 
 - updated TLD files
 - fix: update_tld_files and update installed copies (#76)
-
 
 ### [1.1.0] - 2022-09-29
 
@@ -40,9 +33,8 @@
 - switch http -> https for extra-tlds URL
 - for PSL updater, use project root, #74
 
-
 ### [1.0.34] - 2022-06-05
- 
+
 - ci: reusable workflows from haraka/.github
 - ci: fix publish workflow (#66)
 - ci: workflow tweaks (#64)
@@ -50,40 +42,33 @@
 - ci: update codeclimate config
 - chore: replace some promises with async/await
 
-
 #### 1.0.30 - 2022-05-23
 
 - update TLD files
 - dep(\*): pin major versions
-
 
 #### 1.0.29 - 2021-10-10
 
 - update TLD files
 - upon merge to master, automatically publish new version
 
-
 #### 1.0.28 - 2021-06-09
 
 - update TLD files & bump version
 - allow disabling logging with env.HARAKA_LOGS_SUPPRESS
 
-
 ### 1.0.27 - 2021-01-05
 
 - update TLD files & bump version
-
 
 ### 1.0.26 - 2020-10-23
 
 - bump version
 - replace travis & AppVeyor with GH workflow
 
-
 ### 2019-08-19
 
 - unref interval, so node.js process exits normally (#52)
-
 
 ### 1.0.24 - 2019-04-09
 
@@ -91,121 +76,99 @@
 - assuming no ill effects, after some release validation, extend to the other TLD files
 - and say goodbye to `update TLD files` releases
 
-
 ### 1.0.23 - 2019-03-30
 
-* update TLD files
-
+- update TLD files
 
 ### 1.0.22 - 2018-11-14
 
-* update TLD files
-
+- update TLD files
 
 ### 1.0.21 - 2018-07-20
 
-* update TLD files
-
+- update TLD files
 
 ### 1.0.20 - 2018-06-15
 
-* update TLD files
-
+- update TLD files
 
 ### 1.0.19 - 2018-03-07
 
-* update TLD files
-
+- update TLD files
 
 ### 1.0.18 - 2017-12-30
 
-* update TLD files
-
+- update TLD files
 
 ### 1.0.17 - 2017-09-10
 
-* update TLD files
-
+- update TLD files
 
 ### 1.0.16 - 2017-07-30
 
-* update TLD files
-* dev/CI updates
-
+- update TLD files
+- dev/CI updates
 
 ### 1.0.15 - 2017-06-16
 
-* update TLD files
-* update for eslint4 compat
-
+- update TLD files
+- update for eslint4 compat
 
 ### 1.0.14 - 2017-05-22
 
-* update TLD files
-
+- update TLD files
 
 ### 1.0.13 - Feb 01, 2017
 
-* update TLD files
-* added INSTALL section to README
-* inherit lint rules from eslint-plugin-haraka
-* remove grunt-version-check (replaced by greenkeeper)
-* remove Grunt entirely
-
+- update TLD files
+- added INSTALL section to README
+- inherit lint rules from eslint-plugin-haraka
+- remove grunt-version-check (replaced by greenkeeper)
+- remove Grunt entirely
 
 ### 1.0.12 - Dec 28, 2016
 
-* update TLD files
-
+- update TLD files
 
 ### 1.0.11 - Nov 13, 2016
 
-* update TLD files
-
+- update TLD files
 
 ### 1.0.10 - Oct 06, 2016
 
-* update TLD files
-* update lint / code coverage meta
-
+- update TLD files
+- update lint / code coverage meta
 
 ### 1.0.9 - Aug 22, 2016
 
-* update TLD files
-
+- update TLD files
 
 ### 1.0.7 - Jul 20, 2016
 
-* update TLD files
-* update dep version numbers
-
+- update TLD files
+- update dep version numbers
 
 ### 1.0.6 - Jun 20, 2016
 
-* update TLD files
-* added curl -S (report errors) and -f (don't save files when HTTP errors encountered)
-
+- update TLD files
+- added curl -S (report errors) and -f (don't save files when HTTP errors encountered)
 
 ### 1.0.5 - May 16, 2016
 
-* updated TLD files
-
+- updated TLD files
 
 ### 1.0.4 - Feb 22, 2016
 
-* use `__dirname` for installed base path
-
+- use `__dirname` for installed base path
 
 ### 1.0.2 - Feb 05, 2016
 
-* updated lists
-* added Gruntfile.js
-
+- updated lists
+- added Gruntfile.js
 
 ### 1.0.1 - Feb 05, 2016
 
-* update path to etc dir
-
+- update path to etc dir
 
 [1.0.32]: https://github.com/haraka/haraka-tld/releases/tag/1.0.31
 [1.0.34]: https://github.com/haraka/haraka-tld/releases/tag/1.0.33
--- test/update.js
+++ test/update.js
@@ -1,7 +1,6 @@
-
 const assert = require('assert')
-const fs     = require('fs')
-const path   = require('path')
+const fs = require('fs')
+const path = require('path')
 
 const update = require('../lib/update')
 
@@ -11,77 +10,91 @@ after(function (done) {
 
 describe('getFileStats', function () {
   it('get fs.stats from default existing PSL', function (done) {
-    update.getFileStats().then((stats) => {
-      assert.ok(stats.size > 10000, stats.size)
-      done()
-    }).catch(done);
+    update
+      .getFileStats()
+      .then((stats) => {
+        assert.ok(stats.size > 10000, stats.size)
+        done()
+      })
+      .catch(done)
   })
 
   it('returns null from a missing file', function (done) {
-    update.getFileStats(path.join('etc','nonexist')).then(stats => {
-      assert.equal(stats, null)
-      done();
-    }).catch(done);
+    update
+      .getFileStats(path.join('etc', 'nonexist'))
+      .then((stats) => {
+        assert.equal(stats, null)
+        done()
+      })
+      .catch(done)
   })
 })
 
 describe('isRemoteNewer', function () {
-  this.slow(500);
+  this.slow(500)
   this.timeout(3000)
   it('a HTTP POST returns false if remote file is newer', function (done) {
-    update.isRemoteNewer(null).then(isNewer => {
-      if (isNewer) {
-        assert.equal(isNewer, true)
-      }
-      else {
-        assert.equal(isNewer, false)
-      }
-      done()
-    }).catch(done)
+    update
+      .isRemoteNewer(null)
+      .then((isNewer) => {
+        if (isNewer) {
+          assert.equal(isNewer, true)
+        } else {
+          assert.equal(isNewer, false)
+        }
+        done()
+      })
+      .catch(done)
   })
 
   it('a HTTP POST returns false when remote non-existing', function (done) {
-    update.isRemoteNewer(null, { path: '/invalid/url' }).then(isNewer => {
+    update.isRemoteNewer(null, { path: '/invalid/url' }).then((isNewer) => {
       assert.equal(isNewer, false)
       done()
     })
   })
 
   it.skip('a HTTP POST returns false when local and remote non-existing', function (done) {
-    update.isRemoteNewer('non/exist', { path: '/invalid/url' }).then(isNewer => {
-      assert.equal(isNewer, false)
-      done()
-    }).catch(done)
+    update
+      .isRemoteNewer('non/exist', { path: '/invalid/url' })
+      .then((isNewer) => {
+        assert.equal(isNewer, false)
+        done()
+      })
+      .catch(done)
   })
 })
 
 describe('getWritableStream', function () {
   it('opens a file for writing a stream to', function (done) {
     const filePath = path.join('test', 'fixtures', 'tmpfile')
-    update.getWritableStream(filePath).then(ws => {
-      assert.equal(ws.writable, true);
-      ws.close();
-      done();
-    }).catch(done)
+    update
+      .getWritableStream(filePath)
+      .then((ws) => {
+        assert.equal(ws.writable, true)
+        ws.close()
+        done()
+      })
+      .catch(done)
   })
 
   it('throws when it cannot open file', function (done) {
     const filePath = path.join('test', 'fixtures', 'unwritable', 'tmpfile')
-    update.getWritableStream(filePath)
-      .then(ws => {
-        assert.ok(!ws);  // shouldn't ever get here
-        done();
+    update
+      .getWritableStream(filePath)
+      .then((ws) => {
+        assert.ok(!ws) // shouldn't ever get here
+        done()
       })
       .catch((err) => {
         // console.log(err.message);
-        assert.ok(err.message);
-        done();
+        assert.ok(err.message)
+        done()
       })
   })
 })
 
 describe('download', function () {
-
   const testOpts = {
     hostname: 'raw.githubusercontent.com',
     path: '/haraka/haraka-tld/master/etc/public-suffix-list',
@@ -89,25 +102,29 @@ describe('download', function () {
 
   it('errors if it cannot open tmp file', function (done) {
     const filePath = path.join('test', 'fixtures', 'unwritable', 'test')
-    update.download(filePath, testOpts)
+    update
+      .download(filePath, testOpts)
       .then((installed) => {
-        assert.equal(installed, false); // should never get here
+        assert.equal(installed, false) // should never get here
         done()
       })
       .catch((err) => {
         console.error(err.message)
-        assert.ok(err);
+        assert.ok(err)
         done()
       })
   })
 
   // avoid transient errors, only run these manually
   it.skip('use HTTP GET to fetch newer PSL', function (done) {
-    this.slow(1000);
-    this.timeout(3000);
-    update.download(null, testOpts).then((installed) => {
-      assert.equal(installed, true);
-      done()
-    }).catch(done)
+    this.slow(1000)
+    this.timeout(3000)
+    update
+      .download(null, testOpts)
+      .then((installed) => {
+        assert.equal(installed, true)
+        done()
+      })
+      .catch(done)
   })
 })
